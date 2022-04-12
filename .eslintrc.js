c0f992513e995a2fec80c9986635e3ad321e5425
bryant8888 <javacenter2013@gmail.com>
2022-04-12 22:07:49 +0300

    Finish css layout

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,4 +11,7 @@ module.exports = {
     es6: true,
   },
   extends: ['eslint:recommended', 'google'],
+  rules: {
+    semi: 0,
+  },
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './module'
-import './scss/index.scss'
+import './module';
+import './scss/index.scss';
 
-console.log("Hello world");
+console.log('Hello world');
